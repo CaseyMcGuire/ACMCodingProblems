6f48463e1fa959990757efff0dbd3855b09b1b06
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-08-08 19:28:40 -0700

    Finished Runes in Haskell

--- Runes/haskell/solution.hs
+++ Runes/haskell/solution.hs
@@ -1,10 +1,9 @@
 import qualified Data.Set as Set
+import Data.List
+import Data.Char
 
 data Rune = Rune { leftNum :: String, operation :: Operation, rightNum :: String, answer :: String }
-instance Functor Rune where
-  fmap f (Rune right op left ans) = Rune (fmap f right) op (fmap f left) (fmap f ans)
 
-type Runes = [Rune]
 data Operation = Operation Char
 
 getNums = Set.fromList ['0','1','2','3','4','5','6','7','8','9']
@@ -14,16 +13,54 @@ getOperation (Operation '*') = (*)
 getOperation (Operation '+') = (+)
 getOperation (Operation '-') = (-)
 getOperation _ = error "Unknown operation"
-  
-solveRune (Rune right op left ans) =
-  let possibleNums = Set.toList $ Set.intersection getNums (Set.fromList (right ++ left ++ ans))
+
+--Given a rune, return the character that corresponds to its solution.
+--Note: This will return a null terminator instead of -1 if a solution is not found because
+-- -1 is not a character :)
+solveRune :: Rune -> Char
+solveRune (Rune left op right ans) =
+  let possibleNums = Set.toList $ Set.difference getNums (Set.fromList (right ++ left ++ ans))
+      getLowestPossibleSolution acc x = if acc /= '\0'
+                                        then acc
+                                        else if isValidSolution x
+                                             then x
+                                             else acc
+      --returns true if the given number is a valid solution for this rune
+      isValidSolution num =
+        let strToInt str = read str::Integer
+            replaceQuestionMarks = map (\x -> if x == '?' then num else x)
+            leftNum = replaceQuestionMarks left
+            operand = getOperation op
+            rightNum = replaceQuestionMarks right
+            answer = replaceQuestionMarks ans
+        in strToInt rightNum `operand` strToInt leftNum == strToInt answer
+           && validNumber rightNum
+           && validNumber leftNum
   in if null possibleNums
-     then -1
-     else 0
+     then '\0'
+     else foldl' (\acc x -> getLowestPossibleSolution acc x) '\0' possibleNums
 
-isSolution num (Rune right op left ans) =
-  let dog = "dog"
-  in "dog"
+--Returns true if the given string is a valid number (assuming that all the characters are digits)
+validNumber :: String -> Bool
+validNumber [_] = True
+validNumber ('0':_) = False
+validNumber _ = True
+
+--Converts a valid string to a rune
+--e.g. 123*45?=5?088
+strToRune :: String -> Rune
+strToRune ('-':line) = let (Rune a b c d) = strToRune line in Rune ('-':a) b c d
+strToRune line =
+  let allowedOperations = ['+', '-', '*']
+      (firstNum, rest) = break (\x -> elem x allowedOperations) line
+      op = head rest
+      (secondNum,_:answer) = span (/='=') (tail rest)
+  in Rune firstNum (Operation op) secondNum answer
 
 
-
+main::IO()
+main = do
+  _ <- getLine --ignore number of test cases
+  lines <- fmap lines getContents 
+  let runes = map (\x -> if x == '\0' then -1 else digitToInt x) $ map solveRune $ map strToRune lines
+  mapM_ print $ runes
